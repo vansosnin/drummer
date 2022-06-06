244ad0cd4805166fa0e80be510f0e74c463be3af
Ivan Sosnin <isosnin@skbkontur.ru>
2022-06-06 16:52:13 +0500

    Fix play sounds

--- src/services/audioService.ts
+++ src/services/audioService.ts
@@ -2,62 +2,75 @@ import { SequencerChannelInstrument } from "../Sequencer/types";
 
 const OSCILLATOR_NOTE_LENGTH = 0.1;
 
+const instrumentRelativeUrls = new Map<SequencerChannelInstrument, string>([
+  [SequencerChannelInstrument.HiHat, "/audio/hihat.mp3"],
+  [SequencerChannelInstrument.Snare, "/audio/snare.mp3"],
+  [SequencerChannelInstrument.Kick, "/audio/kick.mp3"],
+]);
+
 class AudioService {
   private readonly audioContext: AudioContext;
   private readonly gainNode: GainNode;
-  private readonly hiHatAudioElement: HTMLAudioElement;
-  private readonly snareAudioElement: HTMLAudioElement;
-  private readonly kickAudioElement: HTMLAudioElement;
+  private readonly instrumentAudioBuffers = new Map<
+    SequencerChannelInstrument,
+    AudioBuffer
+  >();
 
   constructor() {
     this.audioContext = new AudioContext();
     this.gainNode = this.audioContext.createGain();
 
-    const OSCILLATORS_COUNT = 2;
-    this.gainNode.connect(this.audioContext.destination);
-    this.gainNode.gain.value = 1 / (OSCILLATORS_COUNT * 2);
-
-    this.hiHatAudioElement = new Audio(
-      process.env.PUBLIC_URL + "/audio/hihat.mp3"
-    );
-    this.snareAudioElement = new Audio(
-      process.env.PUBLIC_URL + "/audio/snare.mp3"
-    );
-    this.kickAudioElement = new Audio(
-      process.env.PUBLIC_URL + "/audio/kick.mp3"
-    );
+    this.setupOscillators();
+    this.setupInstruments();
   }
 
   public playSound(instrument: SequencerChannelInstrument): void {
     const instruments: Record<SequencerChannelInstrument, () => void> = {
-      [SequencerChannelInstrument.Beep]: this.playBeep,
-      [SequencerChannelInstrument.Boop]: this.playBoop,
-      [SequencerChannelInstrument.HiHat]: this.playHiHat,
-      [SequencerChannelInstrument.Snare]: this.playSnare,
-      [SequencerChannelInstrument.Kick]: this.playKick,
+      [SequencerChannelInstrument.Beep]: () => this.playOscillatorSound(440),
+      [SequencerChannelInstrument.Boop]: () => this.playOscillatorSound(220),
+      [SequencerChannelInstrument.HiHat]: () =>
+        this.playInstrument(SequencerChannelInstrument.HiHat),
+      [SequencerChannelInstrument.Snare]: () =>
+        this.playInstrument(SequencerChannelInstrument.Snare),
+      [SequencerChannelInstrument.Kick]: () =>
+        this.playInstrument(SequencerChannelInstrument.Kick),
     };
 
     instruments[instrument]();
   }
 
-  private playBeep = (): void => {
-    this.playOscillatorSound(440);
+  private setupOscillators() {
+    const OSCILLATORS_COUNT = 2;
+    this.gainNode.connect(this.audioContext.destination);
+    this.gainNode.gain.value = 1 / (OSCILLATORS_COUNT * 2);
+  }
+
+  private async setupInstruments(): Promise<void> {
+    await Promise.all(
+      Array.from(instrumentRelativeUrls.keys()).map(this.setupInstrument)
+    );
+  }
+
+  private setupInstrument = async (
+    instrument: SequencerChannelInstrument
+  ): Promise<void> => {
+    const relativeUrl = instrumentRelativeUrls.get(instrument);
+    const response = await fetch(process.env.PUBLIC_URL + relativeUrl);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await this.audioContext.decodeAudioData(arrayBuffer);
+    this.instrumentAudioBuffers.set(instrument, audioBuffer);
   };
 
-  private playBoop = (): void => {
-    this.playOscillatorSound(220);
-  };
+  private playInstrument = (instrument: SequencerChannelInstrument) => {
+    const bufferSourceNode = this.audioContext.createBufferSource();
+    const audioBuffer = this.instrumentAudioBuffers.get(instrument);
+    if (!audioBuffer) {
+      throw new Error(`Audio buffer not found for instrument ${instrument}`);
+    }
 
-  private playHiHat = (): Promise<void> => {
-    return this.hiHatAudioElement.play();
-  };
-
-  private playSnare = (): Promise<void> => {
-    return this.snareAudioElement.play();
-  };
-
-  private playKick = (): Promise<void> => {
-    return this.kickAudioElement.play();
+    bufferSourceNode.buffer = audioBuffer;
+    bufferSourceNode.connect(this.audioContext.destination);
+    bufferSourceNode.start();
   };
 
   private playOscillatorSound(frequency: number): void {
